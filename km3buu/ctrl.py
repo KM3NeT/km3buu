707786f4440df8287b52e0d99c9263c0a4ae6836
Johannes Schumann <johannes.schumann@fau.de>
2020-03-09 16:27:31 +0100

    Use of tempdir instead of tempfile

--- km3buu/ctrl.py
+++ km3buu/ctrl.py
@@ -14,16 +14,16 @@ __status__ = "Development"
 
 import os
 from spython.main import Client
-from os.path import join, abspath, basename, isdir
+from os.path import join, abspath, basename, isdir, isfile
 from tempfile import NamedTemporaryFile, TemporaryDirectory
 from thepipe.logger import get_logger
 
 from . import IMAGE_NAME
 from .config import Config
+from .jobcard import Jobcard
 
 log = get_logger(basename(__file__))
 
-INPUT_PATH = "/opt/buuinput2019/"
 GIBUU_SHELL = """
 #!/bin/bash
 
@@ -41,30 +41,36 @@ $CONTAINER_GIBUU_EXEC < {1};
 
 def run_jobcard(jobcard, outdir):
     """
-    method for run
+    Method for run
 
     Parameters
     ----------
-    jobcard: km3buu.JobCard
-        The jobcard which should be run
-    outdir: str
-        The path to the directory the output should be written to
+    jobcard: str, km3buu.JobCard
+        The jobcard which should be run, which can be an instance
+        of a jobcard object or a path to a jobcard
+    outdir: str 
+        The path to the directory the output should be written to.
     """
-    tmp_dir = TemporaryDirectory()
+    script_dir = TemporaryDirectory()
     outdir = abspath(outdir)
     log.info("Create temporary file for jobcard")
-    jobcard_fpath = join(tmp_dir.name, "tmp.job")
-    with open(jobcard_fpath, 'w') as f:
-        f.write(str(jobcard))
+    jobcard_fpath = join(script_dir.name, "tmp.job")
+    if isinstance(jobcard, Jobcard):
+        with open(jobcard_fpath, 'w') as f:
+            f.write(str(jobcard))
+    elif isfile(jobcard):
+        os.system("cp %s %s" % (jobcard, jobcard_fpath.name))
+    else:
+        log.error("No valid jobcard reference given: %s" % jobcard)
     log.info("Create temporary file for associated runscript")
-    script_fpath = join(tmp_dir.name, "run.sh")
+    script_fpath = join(script_dir.name, "run.sh")
     with open(script_fpath, 'w') as f:
         ctnt = GIBUU_SHELL.format(outdir, jobcard_fpath)
         f.write(ctnt)
-    os.system("ls %s" % (tmp_dir.name))
+    os.system("ls %s" % (script_dir.name))
     output = Client.execute(Config().gibuu_image_path,
                             ['/bin/sh', script_fpath],
-                            bind=[outdir, tmp_dir.name],
+                            bind=[outdir, script_dir.name],
                             return_result=True)
     msg = output['message']
     if isinstance(msg, str):
