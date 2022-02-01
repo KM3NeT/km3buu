1a79ca619456a7aa678cdd465d6c14a9338215c3
Johannes Schumann <johannes.schumann@fau.de>
2022-02-01 16:21:51 +0100

    Add bjorkeny functions

--- km3buu/swim.py
+++ km3buu/swim.py
@@ -15,13 +15,25 @@ __status__ = "Development"
 import numpy as np
 import ROOT
 
+ENERGY_RANGE = (0.1, 100)
+ENERGY_BINS = 40
+BJORKENY_BINS = 20
+
 
 def build_swim_xsec_label(flavor, process, anti, loglabel=False):
-    f = flavor[0].upper()
+    f = flavor[0].lower()
     p = process.upper()
-    a = "u" if anti else "b"
+    a = "b" if anti else "u"
     l = "log" if loglabel else ""
     return "gn{}{}_{}_{}E".format(a, f, p, l)
+
+
+def build_swim_by_label(flavor, process, anti):
+    flavor_map = {"electron": "e", "muon": "mu", "tau": "tau"}
+    f = flavor_map[flavor]
+    p = process.lower()
+    a = "a" if anti else ""
+    return "h2d_bydist_E_{}nu{}_{}".format(a, f, p)
 
 
 def write_swim_xsec_file(energy,
@@ -34,5 +46,24 @@ def write_swim_xsec_file(energy,
     f.Cd(subdir)
     gr = ROOT.TGraph(len(energy), energy, xsection)
     gr.SetName(label)
-    gr.Write()
+    gr.Write(label, ROOT.TObject.kOverwrite)
+    f.Close()
+
+
+def write_swim_bjorkeny_file(energy, by, label, filename="nu_bjorken_y.root"):
+    f = ROOT.TFile.Open(filename, "UPDATE")
+    f.mkdir("single_graphs")
+    f.Cd("single_graphs")
+    hist = ROOT.TH2D(label, label, ENERGY_BINS, ENERGY_RANGE[0],
+                     ENERGY_RANGE[1], BJORKENY_BINS, 0, 1)
+    z, x, y = np.histogram2d(energy,
+                             by,
+                             bins=(np.linspace(ENERGY_RANGE[0],
+                                               ENERGY_RANGE[1], ENERGY_BINS),
+                                   np.linspace(0, 1, BJORKENY_BINS)),
+                             density=True)
+    for idxE, idxBy in np.ndindex(z.shape):
+        hist.SetBinContent(idxE, idxBy, z[idxE, idxBy])
+
+    hist.Write(label, ROOT.TObject.kOverwrite)
     f.Close()
